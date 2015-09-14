eea63817c94671857b7727700f7f8a41dc786e9e
Andrey Galkin <andrey@egamings.com>
2015-09-14 20:21:37 +0300

    Added project structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'FutoIn CI Tool',
+    'author': 'Andrey Galkin',
+    'url': 'https://github.com/futoin/citool',
+    'download_url': 'https://github.com/futoin/citool/archive/master.zip',
+    'author_email': 'andrey@futoin.org',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['citool'],
+    'scripts': [],
+    'name': 'citool'
+}
+
+setup(**config)
---
+++ tests/citool_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import citool
+
+def setup():
+    pass
+
+def teardown():
+    pass
+
+def test_1():
+    pass
