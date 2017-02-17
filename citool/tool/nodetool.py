f5f040be38c14106304d702a91faae6175394a11
Andrey Galkin <andrey@futoin.org>
2017-02-18 01:55:24 +0200

    Updated Node,js & NVM installation to support external tools and versions

--- citool/tool/nodetool.py
+++ citool/tool/nodetool.py
@@ -1,9 +1,45 @@
 
 from citool.subtool import SubTool
 
+import os
+
 class nodeTool( SubTool ):
     def getType( self ):
         return self.TYPE_RUNTIME
     
     def getDeps( self ) :
         return [ 'nvm' ]
+
+    def _installTool( self, env ):
+        nvm_dir = env['nvmDir']
+        node_version = env['nodeVer']
+        self._callExternal(
+            [ 'bash', '-c',
+              'source {0}/nvm.sh && nvm install {1}' ].format(nvm_dir, node_version) )
+
+    def _initEnv( self, env ) :
+        node_version = env.get('nodeVer', 'stable')
+        env['nodeVer'] = node_version
+
+        nvm_dir = env['nvmDir']
+
+        try:
+            env_to_set = self._callExternal(
+                [ 'bash', '-c',
+                'source {0}/nvm.sh && \
+                nvm use {1} >/dev/null && \
+                env | egrep "(NVM_|\.nvm)"'.format(nvm_dir, node_version) ],
+                verbose = False
+            )
+        except:
+            return
+
+        if env_to_set :
+            env_to_set = env_to_set.split( "\n" )
+
+            for e in env_to_set:
+                if not e: break
+                n, v = e.split('=', 1)
+                os.environ[n] = v
+
+            self._have_tool = True
--- citool/tool/nvmtool.py
+++ citool/tool/nvmtool.py
@@ -1,43 +1,24 @@
 
 from citool.subtool import SubTool
-import subprocess
 import os
 
 class nvmTool( SubTool ):
     "Node Version Manager"
+    NVM_DIR_DEFAULT = os.path.join(os.environ['HOME'], '~/.nvm')
+
     def getType( self ):
         return self.TYPE_RUNENV
 
-    def _installTool( self ):
+    def _installTool( self, env ):
+        nvm_dir = env.get('nvmDir', self.NVM_DIR_DEFAULT)
         self._callExternal(
             [ 'bash', '-c',
-              'test -d ~/.nvm || \
-              git clone https://github.com/creationix/nvm.git ~/.nvm && \
-              cd ~/.nvm && git checkout $(git describe --abbrev=0 --tags)' ] )
-        self._callExternal(
-            [ 'bash', '-c',
-              'source ~/.nvm/nvm.sh && nvm install stable' ] )
+              'git clone https://github.com/creationix/nvm.git {0} && \
+               cd {0} && git checkout $(git describe --abbrev=0 --tags)'.format(nvm_dir) ] )
             
-    def initEnv( self, env ) :
-        try:
-            env_to_set = self._callExternal(
-                [ 'bash', '-c',
-                'source ~/.nvm/nvm.sh && \
-                nvm use stable >/dev/null && \
-                env | egrep "(NVM_|\.nvm)"' ],
-                verbose = False
-            )
-        except:
-            return
-
-        if env_to_set :
-            env_to_set = env_to_set.split( "\n" )
-            
-            for e in env_to_set:
-                if not e: break
-                n, v = e.split('=', 1)
-                os.environ[n] = v
-                
-            self._have_tool = True
+    def _initEnv( self, env ) :
+        nvm_dir = env.get('nvmDir', self.NVM_DIR_DEFAULT)
+        env['nvmDir'] = nvm_dir
+        self._have_tool = os.path.exists(os.path.join(nvm_dir, 'nvm.sh'))
         
         
