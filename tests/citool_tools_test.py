ac40801d8d4a5652e4f3aee42a0c72d28c852a7e
Andrey Galkin <andrey@futoin.org>
2017-02-22 23:05:20 +0200

    Added php system + php-build tests

--- tests/citool_tools_test.py
+++ tests/citool_tools_test.py
@@ -6,13 +6,28 @@ class citool_Tool_UTBase ( citool_UTBase ) :
     __test__ = False
     TOOL_NAME = 'invalid'
     TOOL_MANAGED = True
+    TOOL_ENV = {}
+    _env_backup = None
 
     @classmethod
     def setUpClass( cls ):
+        cls._env_backup = {}
         cls.TEST_DIR = os.path.join(cls.TEST_RUN_DIR, 'tool_'+cls.TOOL_NAME)
         super(citool_Tool_UTBase, cls).setUpClass()
         os.mkdir( cls.TEST_DIR )
         os.chdir( cls.TEST_DIR )
+        
+        for k, v in cls.TOOL_ENV.items():
+            cls._env_backup[k] = os.environ.get(k, None)
+            os.environ[k] = v
+            
+    @classmethod
+    def tearDownClass( cls ):
+        for k, v in cls._env_backup.items():
+            if v:
+                os.environ[k] = v
+            else:
+                del os.environ[k]
         
     def test_10_tool_uninstall( self ):
         if self.TOOL_MANAGED:
@@ -38,6 +53,8 @@ class citool_Tool_UTBase ( citool_UTBase ) :
             self._call_citool( [ 'tool', 'update', self.TOOL_NAME ] )
         
 
+# 10
+#-----
 for t in ['bash', 'curl', 'git', 'hg', 'svn', 'gpg', 'scp', 'ssh']:
     cls = 'citool_Tool_10_' + t
     globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
@@ -46,22 +63,44 @@ for t in ['bash', 'curl', 'git', 'hg', 'svn', 'gpg', 'scp', 'ssh']:
         'TOOL_MANAGED' : False,
     })
     
-for t in ['nvm', 'rvm']: # phpenv, virtualenv
+# 20
+#-----
+for t in ['nvm', 'rvm', 'phpbuild']: # virtualenv
     cls = 'citool_Tool_20_' + t
     globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
         '__test__' : True,
         'TOOL_NAME' : t,
     })
 
+# 30
+#-----
 os.environ['rubyVer'] = 'ruby-2.3.3' # use available pre-built binary
 
-for t in ['node', 'ruby']: # php, python
+for t in ['node', 'ruby']: # python
     cls = 'citool_Tool_30_' + t
     globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
         '__test__' : True,
         'TOOL_NAME' : t,
     })
+    
+cls = 'citool_Tool_30_php'
+globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
+    '__test__' : True,
+    'TOOL_NAME' : t,
+    'TOOL_ENV': {
+        'phpVer' : '7',
+    },
+})
+#--
+cls = 'citool_Tool_30_php_system'
+globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
+    '__test__' : True,
+    'TOOL_NAME' : t,
+    'TOOL_MANAGED' : False,
+})
 
+# 40
+#-----
 for t in ['npm', 'gem']: # pip
     cls = 'citool_Tool_40_' + t
     globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
@@ -71,6 +110,8 @@ for t in ['npm', 'gem']: # pip
     })
 
 
+# 50
+#-----
 for t in ['composer']:
     cls = 'citool_Tool_50_' + t
     globals()[cls] = type(cls, (citool_Tool_UTBase, ), {
