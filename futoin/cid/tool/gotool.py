f046fc2d7046c8b60fd2a3dfdf64af54d8572be4
Andrey Galkin <andrey@futoin.org>
2017-04-01 11:45:26 +0300

    Fixed goTool run support

--- futoin/cid/tool/gotool.py
+++ futoin/cid/tool/gotool.py
@@ -67,3 +67,11 @@ through CID, but you can install source releases through
             self._updateEnvFromOutput(env_to_set)
             super(goTool, self).initEnv( env )
     
+            
+    def onRun( self, config, file, args, tune ):
+        env = config['env']
+        self._callInteractive([
+            env[self._name + 'Bin'], 'run', file
+        ] + args)
+            
+   
