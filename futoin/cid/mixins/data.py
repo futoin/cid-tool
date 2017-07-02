cde64b4a24d75db58c98161fd43d05891f0cc837
Andrey Galkin <andrey@futoin.org>
2017-07-02 13:36:55 +0300

    Fixed missing slots for ServiceMixIn

--- futoin/cid/mixins/data.py
+++ futoin/cid/mixins/data.py
@@ -4,6 +4,7 @@ from .ondemand import OnDemandMixIn
 
 class DataSlots(OnDemandMixIn):
     __slots__ = (
+        # ConfigMixIn
         '_startup_env',
         '_env',
         '_config',
@@ -13,14 +14,22 @@ class DataSlots(OnDemandMixIn):
         '_deploy_config',
         '_project_config',
 
+        # DeployMixIn
         '_current_dir',
         '_devserve_mode',
 
+        # LockMixIn
         '_deploy_lock',
         '_master_lock',
         '_global_lock',
 
+        # ToolMixIn
         '_tool_impl',
 
         '_lastPackages',
+
+        # ServiceMixIn
+        '_running',
+        '_reload_services',
+        '_interruptable',
     )
