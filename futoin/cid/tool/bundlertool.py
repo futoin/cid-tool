62b6f6d463d5be1bce274c81e21686eaf1d48dc7
Andrey Galkin <andrey@futoin.org>
2017-10-12 03:38:36 +0300

    BREAKING CHANGE: redesign of BUNDLE_HOME setup

--- futoin/cid/tool/bundlertool.py
+++ futoin/cid/tool/bundlertool.py
@@ -7,20 +7,52 @@ class bundlerTool(GemToolMixIn, BuildTool):
     """Bundler: The best way to manage a Ruby application's gems.
 
 Home: http://bundler.io/
+
+Note:
+1. It will use per-project BUNDLE_PATH=vendor/bundle by default
+2. In deployment with missing vendor/bundle, it uses per-deployment BUNDLE_PATH
+3. If run outside of project/deployment, standard ~/.bundle is used
 """
     __slots__ = ()
+
+    def envNames(self):
+        return ['bundlePath', 'bundlerVer']
 
     def autoDetectFiles(self):
         return 'Gemfile'
 
     def initEnv(self, env):
+        ospath = self._ospath
+
+        if 'CID_DEPLOY_HOME' in self._environ:
+            if ospath.exists('vendor/bundle'):
+                # packed deps
+                bundlePath = self._ospath.realpath('vendor/bundle')
+            else:
+                # per deployment
+                bundlePath = ospath.join(
+                    self._pathutil.deployHome(), '.bundle')
+        elif not ospath.exists('Gemfile'):
+            # global
+            bundlePath = ospath.join(self._pathutil.deployHome(), '.bundle')
+        else:
+            # per project
+            bundlePath = self._ospath.realpath('vendor/bundle')
+
+        bundlePath = env.setdefault('bundlePath', bundlePath)
+        self._environ['BUNDLE_PATH'] = bundlePath
+
         super(bundlerTool, self).initEnv(env, 'bundle')
 
     def onPrepare(self, config):
-        cmd = [config['env']['bundlerBin'], 'install']
+        cmd = [config['env']['bundlerBin'], 'install', '--quiet']
+
+        if self._ospath.exists('Gemfile.lock'):
+            cmd.append('--deployment')
+
         self._executil.callMeaningful(cmd)
 
     def onPackage(self, config):
-        cmd = [config['env']['bundlerBin'], 'install',
+        cmd = [config['env']['bundlerBin'], 'install', '--quiet',
                '--deployment', '--clean']
         self._executil.callMeaningful(cmd)
