fec994a396e4c2edd4fe191c6f401697a9c53ab8
Andrey Galkin <andrey@futoin.org>
2018-08-18 03:03:31 +0300

    NEW: ctestTool to automatically run CMake tests

---
+++ futoin/cid/tool/ctesttool.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2018 Andrey Galkin <andrey@futoin.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from ..testtool import TestTool
+
+
+class ctestTool(TestTool):
+    """Build, Test and Package Your Software With CMake.
+
+Home: https://cmake.org/
+
+CTest runs tests in CMake build folder, if any.
+"""
+    __slots__ = ()
+
+    def autoDetectFiles(self):
+        return 'CMakeLists.txt'
+    
+    def getDeps(self):
+        return ['cmake']
+
+    def envNames(self):
+        return ['ctestBin']
+
+    def onCheck(self, config):
+        ospath = self._ospath
+        os = self._os
+        build_dir = config['env']['cmakeBuildDir']
+
+        if ospath.exists(build_dir):
+            os.chdir(build_dir)
+            cmd = [config['env']['ctestBin']]
+            self._executil.callMeaningful(cmd)
+            os.chdir(config['wcDir'])
