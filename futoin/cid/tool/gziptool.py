d2b2006962142de4f79fc41f2ed33f870e1a1496
Andrey Galkin <andrey@futoin.org>
2019-07-18 15:03:04 +0300

    CHANGED: updated copyright year

--- futoin/cid/tool/gziptool.py
+++ futoin/cid/tool/gziptool.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2015-2018 Andrey Galkin <andrey@futoin.org>
+# Copyright 2015-2019 Andrey Galkin <andrey@futoin.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
